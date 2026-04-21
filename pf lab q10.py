5ccb51f95f4f8cf37e4d89d16379cab5853a9c31
sandunichethana <sandunichethana07@gmail.com>
2026-04-21 15:22:20 +0530

    Create pf lab q10.py

---
+++ pf lab q10.py
@@ -0,0 +1,8 @@
+while True:
+    count=1
+    total=0
+    while count<= 5:
+        num=int(input("enter the number count"))
+        total += num
+        count +=  1
+        print("total is:",total)
